89cc041f11d662dafdd90560b36d466ecf18fd95
Vineel Sai <mail@vineelsai.com>
2024-01-03 21:56:01 +0530

    add link and static recods

--- AAAA.tf
+++ AAAA.tf
@@ -7,3 +7,23 @@ resource "cloudflare_record" "ip" {
   zone_id = var.zone_id
   comment = var.default_comment
 }
+
+resource "cloudflare_record" "link" {
+  name    = "link"
+  proxied = true
+  ttl     = 1
+  type    = "AAAA"
+  value   = "100::"
+  zone_id = var.zone_id
+  comment = var.default_comment
+}
+
+resource "cloudflare_record" "static" {
+  name    = "static"
+  proxied = true
+  ttl     = 1
+  type    = "AAAA"
+  value   = "100::"
+  zone_id = var.zone_id
+  comment = var.default_comment
+}
