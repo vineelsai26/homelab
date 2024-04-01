eeddfa27a142a1b38b5ec6ba04f7057ab1a045ed
Vineel Sai <mail@vineelsai.com>
2024-04-01 21:23:49 +0530

    TrueNAS backup s3 bucket

--- terraform/AWS/Backup/s3.tf
+++ terraform/AWS/Backup/s3.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "github_backup" {
     Environment = "Terraform"
   }
 }
+
+resource "aws_s3_bucket" "truenas_backup" {
+  bucket = "vineelsai-truenas-backup"
+
+  tags = {
+    Name        = "S3 Bucket for TrueNAS Backup"
+    Environment = "Terraform"
+  }
+}
